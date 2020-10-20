2e2de60a0ac80d96c00cf244ab0ff6d9a79f11c5
Artem Kylymnyk <artyomki@gmail.com>
2020-10-20 12:38:44 +0000

    Day 1 Task

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Let's have a short quiz.\n"+
+    "================================");
+       
+    Scanner scan=new Scanner(System.in);
+    
+    System.out.println("What's your name?");
+    String userName1 = scan.nextLine();
+    System.out.println("What's your name?");
+    String userName2 = scan.nextLine();
+    System.out.println("What's your name?");
+    String userName3 = scan.nextLine();
+    
+    System.out.println("Hello "+userName1+"!");
+    System.out.println("Hello "+userName2+"!");
+    System.out.println("Hello "+userName3+"!");
+    scan.close();
+    
+    
   }
 }
